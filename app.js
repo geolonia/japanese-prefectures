b15a2b6ea23bd13ee7f8873c51ba52d0a8ced551
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-02-27 05:14:17 +0900

    some fix

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 ( async () => {
-    const url = "./japanese-prefectures.svg"
-    const res = await fetch( url )
+  const url = "./japanese-prefectures.svg"
+  const res = await fetch( url )
 
-    if ( res.ok ) {
-        const svg = await res.text()
-        document.getElementById( 'map' ).innerHTML = svg
-        const prefs = document.querySelectorAll('#japanese-prefectures .prefecture')
-        prefs.forEach( (pref) => {
-            pref.addEventListener( 'mouseover', (event) => {
-                event.currentTarget.style.fill = "#ff0000"
-            })
-            pref.addEventListener( 'mouseleave', (event) => {
-                event.currentTarget.style.fill = ""
-            })
-        } )
-    }
+  if ( res.ok ) {
+    const svg = await res.text()
+    document.getElementById( 'map' ).innerHTML = svg
+    const prefs = document.querySelectorAll('#japanese-prefectures .prefecture')
+    prefs.forEach( (pref) => {
+      pref.addEventListener( 'mouseover', (event) => {
+        event.currentTarget.style.fill = "#ff0000"
+      })
+      pref.addEventListener( 'mouseleave', (event) => {
+        event.currentTarget.style.fill = ""
+      })
+    } )
+  }
 } )()
