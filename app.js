18be46915e0d941060706e14119cbd446d69319d
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-02-27 19:36:37 +0900

    update preview

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 ( async () => {
-  const url = "./map-small.svg"
-  const res = await fetch( url )
+  const maps = [ "./map-full.svg", "./map-mobile.svg" ]
+  const containers = document.querySelectorAll( '.map' )
 
-  if ( res.ok ) {
-    const svg = await res.text()
-    document.getElementById( 'map' ).innerHTML = svg
-    const prefs = document.querySelectorAll('#map .prefecture')
-    prefs.forEach( (pref) => {
-      pref.addEventListener( 'mouseover', (event) => {
-        event.currentTarget.style.fill = "#ff0000"
-      })
-      pref.addEventListener( 'mouseleave', (event) => {
-        event.currentTarget.style.fill = ""
-      })
-    } )
-  }
+  maps.forEach( async ( map, index ) => {
+    const res = await fetch( map )
+
+    if ( res.ok ) {
+      const svg = await res.text()
+      containers[ index ].innerHTML = svg
+      const prefs = document.querySelectorAll('.prefecture')
+      prefs.forEach( (pref) => {
+        pref.addEventListener( 'mouseover', (event) => {
+          event.currentTarget.style.fill = "#ff0000"
+        })
+        pref.addEventListener( 'mouseleave', (event) => {
+          event.currentTarget.style.fill = ""
+        })
+      } )
+    }
+  } )
 } )()
